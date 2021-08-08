820b8c364cf771a142e7a6d89cfe5781aa2cecc2
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2021-08-08 17:22:13 +0530

    updating   contact

--- src/client/data/bannerData.js
+++ src/client/data/bannerData.js
@@ -1,5 +1,5 @@
 const BANNER_DATA = {
-    heading:"Naseeb sports Academy For Children And Veteran",
+    heading:"Naseeb sports Academy For Children And Veterans",
 description :"Sports Club Of Tennis And Cricket where you learn to play  from experienced and qualified coaches",
 bannerBtnText:"Contact Us"
 }
